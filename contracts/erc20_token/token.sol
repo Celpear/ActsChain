26e52ea4ba8515ef8d3e4e51e09c0860ec39e9c9
KevMi <kevin.miederer@celpear.com>
2024-03-01 09:58:22 +0100

    add deployment address

--- contracts/erc20_token/token.sol
+++ contracts/erc20_token/token.sol
@@ -9,23 +9,18 @@ import "@0x/contracts-erc20/contracts/src/ERC20Token.sol";
  * @dev ActsChain is an open-source Ethereum token project dedicated to providing GPU power for decentralized face recognition services.
  */
 contract ActsChainCoin is ERC20Token {
-
+    //current deployment on Sepolia: 0x8FA84d8D434792bE64b00c8f6fFaf244146d3D35
     string public name;
     string public symbol;
     uint256 public decimals;
 
-    constructor (
-        string memory _name,
-        string memory _symbol,
-        uint256 _decimals,
-        uint256 _totalSupply
-    )
+    constructor ()
         public
     {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        _totalSupply = _totalSupply;
+        name = "ActsChain";
+        symbol = "ACTS";
+        decimals = 18;
+        _totalSupply = 100000000000000000000000000;
         balances[msg.sender] = _totalSupply;
     }
 }
